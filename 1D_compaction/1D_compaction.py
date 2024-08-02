6ff7f60f92d163260b08ad4718bfaff7fc857f14
Yidong-ZHAO <ydzhao94@gmail.com>
2024-08-02 09:42:19 +0900

    Cleaning the code structure

---
+++ 1D_compaction/1D_compaction.py
@@ -0,0 +1,89 @@
+import sys
+sys.path.append('..')
+
+from mpm.simulator import SimulatorQuasiStatic
+
+import warp as wp
+import numpy as np
+
+import meshio
+
+# MPM setup parameters
+# Grid quantities
+n_grid_x = 20
+n_grid_y = 20
+max_x = 100.0
+dx = max_x/n_grid_x
+inv_dx = float(n_grid_x/max_x)
+
+# Material point quantities
+n_particles = 40
+start_x = dx
+end_x = start_x + dx
+start_y = dx
+end_y = start_y + 50.0
+PPD = 2 # Particles per direction
+p_vol = (dx/PPD)**2 
+p_rho = 0.08 # t/m^3
+youngs_modulus = 10.0 # kPa
+poisson_ratio = 0.0
+
+# Solver
+n_iter = 6
+tol = 1e-10
+
+
+
+# Set boundary dofs
+@wp.kernel
+def set_boundary_dofs(boundary_flag_array: wp.array(dtype=wp.bool),
+                      n_grid_x: wp.int32,
+                      n_nodes: wp.int32):
+    
+    node_idx, node_idy = wp.tid()
+    dof_x = node_idx + node_idy*(n_grid_x + 1)
+    dof_y = dof_x + n_nodes
+
+    # Modify Dirichlet B.C. accordingly
+    if node_idx<=1 or node_idx>=2:
+        boundary_flag_array[dof_x] = True
+
+    if node_idy<=1:
+        boundary_flag_array[dof_y] = True
+
+
+
+sim = SimulatorQuasiStatic(
+				 n_grid_x=n_grid_x,
+                 n_grid_y=n_grid_y,
+                 max_x=max_x,
+                 n_iter=n_iter,
+                 n_particles=n_particles,
+                 start_x=start_x,
+                 end_x=end_x,
+                 start_y=start_y,
+                 end_y=end_y,
+                 PPD=PPD,
+                 p_vol=p_vol,
+                 p_rho=p_rho,
+                 youngs_modulus=youngs_modulus,
+                 poisson_ratio=poisson_ratio,
+                 boundary_function_warp=set_boundary_dofs,
+                 tol=tol
+                 )
+
+# Initial particle configuration
+x_numpy = np.array(sim.x_particles.numpy())
+output_particles = meshio.Mesh(points=x_numpy, cells=[], point_data={'stress_yy': sim.particle_Cauchy_stress_array.numpy()[:,1,1]})
+output_particles.write("./vtk/1d_compaction_particles_%d.vtk" % (0))
+
+n_steps = 40
+for step in range(n_steps):
+    print('Load step:', step+1)
+    sim.advance_one_step(step, n_steps)
+
+	# Post-processing
+    x_numpy = np.array(sim.x_particles.numpy())
+    output_particles = meshio.Mesh(points=x_numpy, cells=[], point_data={'stress_yy': sim.particle_Cauchy_stress_array.numpy()[:,1,1]})
+    output_particles.write("./vtk/1d_compaction_particles_%d.vtk" % (step+1))
+
---
+++ mpm/simulator.py
@@ -0,0 +1,185 @@
+import warp as wp
+import warp.sparse as wps
+import warp.optim.linear
+
+
+import numpy as np
+
+
+from mpm.mpm_utils import init_particles_rectangle, initialization, P2G, assemble_residual, assemble_Jacobian_coo_format, from_increment_to_solution, G2P
+
+
+@wp.struct
+class DofStruct:
+    activate_flag_array: wp.array(dtype=wp.bool)
+    boundary_flag_array: wp.array(dtype=wp.bool)
+
+
+class SimulatorQuasiStatic:
+    def __init__(self,
+                 n_grid_x,
+                 n_grid_y,
+                 max_x,
+                 n_iter,
+                 n_particles,
+                 start_x,
+                 end_x,
+                 start_y,
+                 end_y,
+                 PPD,
+                 p_vol,
+                 p_rho,
+                 youngs_modulus,
+                 poisson_ratio,
+                 boundary_function_warp,
+                 tol
+        ):
+        # Grid quantities
+        self.n_grid_x = n_grid_x
+        self.n_grid_y = n_grid_y
+        self.n_nodes = (self.n_grid_x+1) * (self.n_grid_y+1)
+        self.max_x = max_x
+        self.dx = self.max_x/self.n_grid_x
+        self.inv_dx = float(self.n_grid_x/self.max_x)
+        
+        # Dof quantities
+        self.n_matrix_size = 2 * self.n_nodes # 2 indicates the spatial dimension
+        self.dofStruct = DofStruct()
+        self.dofStruct.activate_flag_array = wp.zeros(shape=self.n_matrix_size, dtype=wp.bool)
+        self.dofStruct.boundary_flag_array = wp.zeros(shape=self.n_matrix_size, dtype=wp.bool)
+
+        self.bsr_matrix = wps.bsr_zeros(self.n_matrix_size, self.n_matrix_size, block_type=wp.float64)
+        self.rhs = wp.zeros(shape=self.n_matrix_size, dtype=wp.float64, requires_grad=True)
+        self.increment = wp.zeros(shape=self.n_matrix_size, dtype=wp.float64)
+        self.old_solution = wp.zeros(shape=self.n_matrix_size, dtype=wp.float64)
+        self.new_solution = wp.zeros(shape=self.n_matrix_size, dtype=wp.float64, requires_grad=True)
+
+        self.rows = wp.zeros(shape=2*25*self.n_matrix_size, dtype=wp.int32) # 2 indicates the spatial dimension
+        self.cols = wp.zeros(shape=2*25*self.n_matrix_size, dtype=wp.int32)
+        self.vals = wp.zeros(shape=2*25*self.n_matrix_size, dtype=wp.float64)
+
+        # Solver
+        self.n_iter = n_iter
+        self.boundary_function_warp = boundary_function_warp
+        self.tol = tol
+
+
+        # Material point quantities
+        self.n_particles = n_particles
+        self.start_x = start_x
+        self.end_x = end_x
+        self.start_y = start_y
+        self.end_y = end_y
+        self.PPD = PPD
+        self.p_vol = p_vol
+        self.p_rho = p_rho
+
+        particles_pos_np = init_particles_rectangle(self.start_x, self.start_y, self.end_x, self.end_y, self.dx, self.n_grid_x, self.n_grid_y, self.PPD, self.n_particles)
+        self.x_particles = wp.from_numpy(particles_pos_np, dtype=wp.vec2d)
+        self.x0_particles = wp.from_numpy(particles_pos_np, dtype=wp.vec2d)
+        self.deformation_gradient = wp.zeros(shape=self.n_particles, dtype=wp.mat33d)
+        self.particle_Cauchy_stress_array = wp.zeros(shape=self.n_particles, dtype=wp.mat33d)
+
+        # Material properties
+        self.youngs_modulus = youngs_modulus
+        self.poisson_ratio = poisson_ratio
+        self.lame_lambda = youngs_modulus*poisson_ratio / ((1.0+poisson_ratio) * (1.0-2.0*poisson_ratio))
+        self.lame_mu = youngs_modulus / (2.0*(1.0+poisson_ratio))
+
+
+        # Initialization
+        wp.launch(kernel=initialization,
+                  dim=self.n_particles,
+                  inputs=[self.deformation_gradient])
+
+        # Specify Dirichlet boundary conditions
+        wp.launch(kernel=self.boundary_function_warp,
+                  dim=(self.n_grid_x+1, self.n_grid_y+1),
+                  inputs=[self.dofStruct.boundary_flag_array, self.n_grid_x, self.n_nodes])
+
+
+
+    def reset_grid_quantities(self):
+        self.old_solution.zero_()
+        self.new_solution.zero_()
+        self.increment.zero_()
+
+
+    def newton_iter(self, current_step, total_step):
+
+        activate_flag_array_np = self.dofStruct.activate_flag_array.numpy()
+
+        for iter_id in range(self.n_iter):
+            self.rhs.zero_()
+
+            tape = wp.Tape()
+            with tape:
+                # assemble residual
+                wp.launch(kernel=assemble_residual,
+                          dim=self.n_particles,
+                          inputs=[self.x_particles, self.inv_dx, self.dx, self.rhs, self.new_solution, self.old_solution, self.p_vol, self.p_rho, self.lame_lambda, self.lame_mu, self.deformation_gradient, self.n_grid_x, self.n_nodes, self.dofStruct.boundary_flag_array, current_step, total_step])
+
+            # Assemble the Jacobian matrix using auto-differentiation
+            for dof_iter in range(self.n_matrix_size): # Loop on dofs
+                if activate_flag_array_np[dof_iter]==False: # This is important for efficient assemblage
+                    continue
+
+                # get the gradient of the rhs[dof] w.r.t. the solution vector. Refer to: https://nvidia.github.io/warp/modules/differentiability.html#jacobians
+                select_index = np.zeros(self.n_matrix_size)
+                select_index[dof_iter] = 1.0
+                e = wp.array(select_index, dtype=wp.float64)
+
+                tape.backward(grads={self.rhs: e})
+                q_grad_i = tape.gradients[self.new_solution]
+
+                # assemble the Jacobian matrix in COOrdinate format
+                wp.launch(kernel=assemble_Jacobian_coo_format,
+                          dim=self.n_matrix_size,
+                          inputs=[q_grad_i, self.rows, self.cols, self.vals, self.n_grid_x, self.n_grid_y, self.n_nodes, self.n_matrix_size, dof_iter, self.dofStruct.boundary_flag_array])
+
+                tape.zero()
+
+
+            # Create sparse matrix from a corresponding COOrdinate (a.k.a. triplet) format
+            wps.bsr_set_from_triplets(self.bsr_matrix, self.rows, self.cols, self.vals)
+
+            # Solve
+            preconditioner = wp.optim.linear.preconditioner(self.bsr_matrix, ptype='diag')
+            solver_state = wp.optim.linear.bicgstab(A=self.bsr_matrix, b=self.rhs, x=self.increment, tol=1e-10, M=preconditioner)
+
+            # From increment to solution
+            wp.launch(kernel=from_increment_to_solution,
+                      dim=self.n_matrix_size,
+                      inputs=[self.increment, self.new_solution])
+
+            with np.printoptions(threshold=np.inf):
+                print('residual.norm:', np.linalg.norm(self.rhs.numpy()))
+
+
+            if np.linalg.norm(self.rhs.numpy())<self.tol:
+                break
+
+
+
+
+    def advance_one_step(self, current_step, total_step):
+        self.reset_grid_quantities()
+
+        # P2G
+        wp.launch(kernel=P2G,
+                  dim=self.n_particles,
+                  inputs=[self.x_particles, self.inv_dx, self.dx, self.n_grid_x, self.n_nodes, self.dofStruct.activate_flag_array])
+
+
+
+        # Newton iteration
+        self.newton_iter(current_step, total_step)
+
+        # G2P
+        wp.launch(kernel=G2P,
+              dim=self.n_particles,
+              inputs=[self.x_particles, self.deformation_gradient, self.particle_Cauchy_stress_array, self.inv_dx, self.dx, self.lame_lambda, self.lame_mu, self.n_grid_x, self.n_nodes, self.new_solution, self.old_solution])
+
+        # # Calculate error
+
+
